49ebb0438bc4f5673d08e7d426be4ef95cf5367a
lupatelles <gabriel.telles0000@gmail.com>
2026-01-13 10:52:48 -0300

    enviando todas as configcloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "cmsg96",
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {}
+  // ...rest of the Cypress project config
 })
